935dbf33048ce5f4364abb1335d2a10244a12a28
Brian Schwedock <schwedoc@usc.edu>
2017-02-09 02:38:56 -0800

    Added camera stuff.  Not working yet, but getting closer.

---
+++ camera/setup.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+## Dependencies to run argus samples
+# cmake, build-essential, pkg-config, X11, GTK3, expat, JPEG, gstreamer-1.0
+apt-get install cmake build-essential pkg-config libx11-dev \
+	libgtk-3-dev libexpat1-dev libjpeg-dev libgstreamer1.0-dev
