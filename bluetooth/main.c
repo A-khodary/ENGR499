bf7dbba382665a3d5046c9d574ff26887a354c92
Zijian Hu <zijianhu@usc.edu>
2017-02-21 03:24:20 -0800

    add menu

--- bluetooth/main.c
+++ bluetooth/main.c
@@ -1,6 +1,8 @@
 #include "lib/config.h"
 #include "lib/scan.h"
 
+void print_menu();
+
 int main(int argc, char **argv)
 {
 	inquiry_info *ii = NULL;
@@ -9,25 +11,59 @@ int main(int argc, char **argv)
 	char addr[19] = { 0 };
 	char name[248] = { 0 };
 
-	if (bt_scan(&ii, max_rsp, &num_rsp, &dev_id, &sock)) {
-		exit(1);
-	}
+	char* input = (char*)malloc(sizeof(char) * 10);
+	int choice = -1;
 
-	int i;
-	for (i = 0; i < num_rsp; i++) {
-		ba2str(&(ii + i)->bdaddr, addr);
-		memset(name, 0, sizeof(name));
+	while (choice != 0) {
+		print_menu();
+		scanf("%s", input);
 
-		if (hci_read_remote_name(sock, &(ii + i)->bdaddr, sizeof(name),
-			name, 0) < 0) {
-			printf("%s  [unknown]\n", addr);
-		}
-		else {
-			printf("%s  %s\n", addr, name);
+		choice = atoi(input);
+
+		switch (choice)
+		{
+		case 1:
+			if (bt_scan(&ii, max_rsp, &num_rsp, &dev_id, &sock)) {
+				exit(1);
+			}
+
+			int i;
+			for (i = 0; i < num_rsp; i++) {
+				ba2str(&(ii + i)->bdaddr, addr);
+				memset(name, 0, sizeof(name));
+
+				if (hci_read_remote_name(sock, &(ii + i)->bdaddr, sizeof(name),
+					name, 0) < 0) {
+					printf("%s  [unknown]\n", addr);
+				}
+				else {
+					printf("%s  %s\n", addr, name);
+				}
+			}
+			break;
+		case 2:
+			if (ii == NULL) {
+				choice = 1;
+				continue;
+			}
+			break;
+		case 3:
+			break;
+		default:
+			break;
 		}
 	}
 
 	free(ii);
+	free(input);
 	close(sock); // a system call that is used to close an open file descriptor
 	return 0;
+}
+
+void print_menu() {
+	printf("What to do?\n");
+	printf("1. Search for Bluetooth devices\n");
+	printf("2. Send data to other devices\n");
+	printf("3. Wait for data from other devices\n");
+	printf("Enter your choice (any invalid input will exit the program: ");
 }
