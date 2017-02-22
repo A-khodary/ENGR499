105301c12749d34f48ccff0d4c22d8408376b96c
Brian Schwedock <schwedoc@usc.edu>
2017-02-22 20:07:12 +0000

    Taking jpg image with usb camera

--- camera/usbcamera/usbcamera.cpp
+++ camera/usbcamera/usbcamera.cpp
@@ -7,15 +7,27 @@ int main()
 {
   //cv::Mat img = cv::imread("example.jpg");
   cv::Mat img;
-  cv::VideoCapture cap("gst-launch-1.0 nvcamerasrc ! 'video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)I420, framerate=(fraction)30/1' ! nvtee ! nvoverlaysink -e");
+  //cv::VideoCapture cap("gst-launch-1.0 nvcamerasrc ! 'video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)I420, framerate=(fraction)30/1' ! nvtee ! nvoverlaysink -e");
 
+  cv::VideoCapture cap(0);
+  
   if (!cap.isOpened())
 	{
 	  std::cout << "video capture is not opened" << std::endl;
 	  return 0;
 	}
 
-  //cv::VideoCapture cap(0);
+  if (!cap.read(img))
+    {
+      std::cout << "Read failed" << std::endl;
+      return 0;
+    }
+
+  imshow("img", img);
+  imwrite("img.jpg", img);
+  cv::waitKey();
+  
+  /*
   
   for (;;)
 	{
@@ -34,4 +46,5 @@ int main()
 		  break;
 		}
 	}
+  */
 }
