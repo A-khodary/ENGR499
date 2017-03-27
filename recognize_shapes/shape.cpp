349b3d976ab8c83300ae5ac00de1b81ac9e8fa22
Brian Schwedock <schwedoc@usc.edu>
2017-03-27 19:41:19 +0000

    Issue with shape angle

--- recognize_shapes/shape.cpp
+++ recognize_shapes/shape.cpp
@@ -3,6 +3,8 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
+#include <iomanip>
+
 cv::Mat GetImage(cv::VideoCapture& cap)
 {
     // Take image
@@ -65,68 +67,111 @@ void RecognizeShapes(cv::Mat shapeImg)
     */
 
     std::vector<cv::Point> approxShape;
+    int pos3 = -180, pos4 = -1, pos5 = -1;
     for (int i = 0; i < contours.size(); i++)
-    {
-	approxPolyDP(contours[i], approxShape,
+    {	
+      	approxPolyDP(contours[i], approxShape,
 		     arcLength(cv::Mat(contours[i]), true) * 0.05, true);
 
-	if (approxShape.size() == 4 && cv::contourArea(approxShape) > 50)
+	if (approxShape.size() >= 3 && cv::contourArea(approxShape) > 50
+	    && cv::contourArea(approxShape) < 1000)
 	{
-	    double perimeter = cv::arcLength(approxShape, true);
-	    double sideLength = perimeter / 4;
-	    double approxArea = pow(sideLength, 2);
+	    if (approxShape.size() == 3)
+	    {
+		double perimeter = cv::arcLength(approxShape, true);
+		double sideLength = perimeter / 3;
+		double approxArea = 1.73205 / 4 * pow(sideLength, 2);
 
-	    double ratio = approxArea / cv::contourArea(approxShape);
-	    if (ratio > 0.95 && ratio <  1.05)
-	    {
-		drawContours(drawing, contours, i, cv::Scalar(0, 255, 255), CV_FILLED);
-		std::vector<cv::Point>::iterator vertex;
-		for (vertex = approxShape.begin(); vertex != approxShape.end(); vertex++)
+		double ratio = approxArea / cv::contourArea(approxShape);
+		if (ratio > 0.9 && ratio <  1.1)
 		{
-		    cv::circle(drawing, *vertex, 3, cv::Scalar(0, 0, 255), 1);
+		    drawContours(drawing, contours, i, cv::Scalar(255, 0, 255), CV_FILLED);
+		    std::vector<cv::Point>::iterator vertex;
+		    for (vertex = approxShape.begin(); vertex != approxShape.end(); vertex++)
+		    {
+			cv::circle(drawing, *vertex, 3, cv::Scalar(0, 0, 255), 1);
+		    }
+		    pos3 = (approxShape[0].x + approxShape[1].x) / 2;
 		}
 	    }
-	}
-	
-	/*if (approxShape.size() > 2 && cv::contourArea(approxShape) > 2500)
-	{
-	    if (approxShape.size() == 4)
+	    else if (approxShape.size() == 4)
 	    {
-		//std::cout << "Area: " << cv::contourArea(approxShape) << std::endl;
-		
-		drawContours(drawing, contours, i, cv::Scalar(0, 255, 255), CV_FILLED);
-		std::vector<cv::Point>::iterator vertex;
-		for (vertex = approxShape.begin(); vertex != approxShape.end(); vertex++)
+		double perimeter = cv::arcLength(approxShape, true);
+		double sideLength = perimeter / 4;
+		double approxArea = pow(sideLength, 2);
+
+		double ratio = approxArea / cv::contourArea(approxShape);
+		if (ratio > 0.95 && ratio <  1.05)
 		{
-		    cv::circle(drawing, *vertex, 3, cv::Scalar(0, 0, 255), 1);
-		}
-	    }
-	    else if (approxShape.size() == 3)
-	    {
-		//std::cout << "Area: " << cv::contourArea(approxShape) << std::endl;
-		
-		drawContours(drawing, contours, i, cv::Scalar(255, 0, 255), CV_FILLED);
-		std::vector<cv::Point>::iterator vertex;
-		for (vertex = approxShape.begin(); vertex != approxShape.end(); vertex++)
-		{
-		    cv::circle(drawing, *vertex, 3, cv::Scalar(0, 0, 255), 1);
+		    drawContours(drawing, contours, i, cv::Scalar(0, 255, 255), CV_FILLED);
+		    std::vector<cv::Point>::iterator vertex;
+		    for (vertex = approxShape.begin(); vertex != approxShape.end(); vertex++)
+		    {
+			cv::circle(drawing, *vertex, 3, cv::Scalar(0, 0, 255), 1);
+		    }
+		    pos4 = (approxShape[0].x + approxShape[1].x) / 2;
 		}
 	    }
 	    else if (approxShape.size() == 5)
 	    {
-		//std::cout << "Area: " << cv::contourArea(approxShape) << std::endl;
-		
-		drawContours(drawing, contours, i, cv::Scalar(255, 255, 0), CV_FILLED);
-		std::vector<cv::Point>::iterator vertex;
-		for (vertex = approxShape.begin(); vertex != approxShape.end(); vertex++)
+		double perimeter = cv::arcLength(approxShape, true);
+		double sideLength = perimeter / 5;
+		double approxArea = 0.25 * 6.8819 * pow(sideLength, 2);
+
+		double ratio = approxArea / cv::contourArea(approxShape);
+		if (ratio > 0.95 && ratio <  1.05)
 		{
-		    cv::circle(drawing, *vertex, 3, cv::Scalar(0, 0, 255), 1);
+		    drawContours(drawing, contours, i, cv::Scalar(255, 255, 0), CV_FILLED);
+		    std::vector<cv::Point>::iterator vertex;
+		    for (vertex = approxShape.begin(); vertex != approxShape.end(); vertex++)
+		    {
+			cv::circle(drawing, *vertex, 3, cv::Scalar(0, 0, 255), 1);
+		    }
+		    pos5 = (approxShape[0].x + approxShape[1].x) / 2;
 		}
 	    }
-	    }*/
-	
+	}	
     }
 
+    // Detect angle of shape
+    pos3 -= 340;
+    pos4 -= 340;
+    pos5 -= 340;
+    
+    double angle3 = (double) pos3 / 340 * 37.5;
+    double angle4 = (double) pos4 / 340 * 37.5;
+    double angle5 = (double) pos5 / 340 * 37.5;
+    
+    std::string string3, string4, string5;
+    if (pos3 == -341)
+    {
+	string3 = "X";
+    }
+    else
+    {
+	string3 = std::to_string(angle3);
+    }
+    if (pos4 == -341)
+    {
+	string4 = "X";
+    }
+    else
+    {
+	string4 = std::to_string(angle4);
+    }
+    if (pos5 == -341)
+    {
+	string5 = "X";
+    }
+    else
+    {
+	string5 = std::to_string(angle3);
+    }
+    
+    std::cout << "angle3:" << std::setw(13) << string3 << ", angle4:"
+	      << std::setw(13) << string4 << ", angle5:"
+	      << std::setw(13) << string5 << '\r';
+    
     imshow("Result window", drawing);
 }
 
@@ -145,7 +190,7 @@ int main()
     if (!cap.set(CV_CAP_PROP_FRAME_HEIGHT, 1080))
 	std::cout << "Resizing failed" << std::endl;
     
-    
+    //std::cout << std::endl;
     while (true)
     {
 	cv::Mat shapeImg = GetImage(cap);
