d0fdecb4a0b4f2e70354a1808ed98c543a17b56f
Cathy <jicathyh@gmail.com>
2017-03-29 09:44:28 -0700

    threaded qr scanner

---
+++ qr_codes_thread/camera.h
@@ -0,0 +1,26 @@
+// Class for taking pictures
+#ifndef CAMERA
+#define CAMERA
+
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <zbar.h>
+
+class Camera
+{
+ public:
+    Camera(int deviceNum);
+    cv::Mat TakePicture();
+    cv::Mat ConvertToBinary(cv::Mat img);
+    void decodeFrame(cv::Mat frame);
+    void configureScanner();
+    
+ private:
+    int imgCounter;
+    std::string filePrefix, fileSuffix;
+    cv::VideoCapture cap;
+    zbar::ImageScanner scanner;
+};
+
+#endif
