1f7fa4d8246cf8a9d3d6286cbd6d8d817f8b1010
Charles Wu <charleyw@usc.edu>
2017-04-16 17:21:17 -0700

    Got orientation as well as sharing data between threads

--- main/include/imu.h
+++ main/include/imu.h
@@ -1,6 +1,16 @@
 #ifndef IMU
 #define IMU
 
-void IMUExecution();
+#include <mutex>
+struct imuInfo{
+ 	float x;
+	float y;
+	float z;
+	float roll;
+	float pitch;
+	float yaw;
+};
+
+void IMUExecution(struct imuInfo &data, std::mutex &dataLock);
 
 #endif
