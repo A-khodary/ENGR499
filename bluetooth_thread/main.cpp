07285e9908def39e9e2fa1aadfed3d3886a59924
Zijian Hu <zijianhu@usc.edu>
2017-04-21 14:40:54 -0700

    Rewrote the bluetooth code to simplify. also found out you can't have multiple send connection requests at the same time. Seemed like 4 seconds apart was the closest they could be.

--- bluetooth_thread/main.cpp
+++ bluetooth_thread/main.cpp
@@ -1,244 +1,220 @@
-#include "lib/config.h"
-#include "lib/scan.h"
-#include "lib/rfcomm.h"
-#include "lib/prompt.h"
-#include "bluetooth.h"
-
 #include <iostream>
 #include <thread>
 #include <functional>
 #include <mutex>
 #include <condition_variable>
 #include <stdexcept>
+#include <vector>
+#include <deque>
+#include <bluetooth/bluetooth.h>
+#include <sys/socket.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/hci_lib.h>
+#include <unistd.h>
+#include <bluetooth/rfcomm.h>
+#include <ctime>
+#include <stdlib.h>
 
-using namespace std;
+void runBluetoothReceive();
+void runBluetoothSend();
 
-void print_menu();
-void exchangeMsgs(deque<string>&, deque<string>&);
-
-void runBluetoothSend(deque<string>&, string, Bluetooth&, int);
-void runBluetoothReceive(deque<string>&, Bluetooth&, int);
-
-mutex mtx;
-condition_variable bt_send, bt_send2, main_cv;
+std::mutex mtx;
+std::condition_variable bt_send;
+std::condition_variable bt_receive;
+std::deque<std::string> send_msgs;
+std::deque<std::string> received_msgs;
+std::vector<std::string> destinations;
 
 int main(int argc, char **argv)
 {
+    std::vector<std::thread> threads;
 
-	if (argc < 1) {
-		cerr << "Error, correct format: ./<executable name> $(./getMyBtAddr.sh)" << endl;
-	}
-	string myAddr(argv[1]);
-	cout << "My addresss is " << myAddr << endl;
+    srand(time(nullptr));
+    
+    if (argc < 1) {
+	std::cerr << "Error, correct format: ./<executable name> $(./getMyBtAddr.sh)" << std::endl;
+    }
+    
+    std::string myAddr(argv[1]);
+    std::cout << "My addresss is " << myAddr << std::endl;
+    
+    std::string s = "00:04:4B:65:C3:75";
+    if ( s != myAddr ) {
+	destinations.push_back(s);
+    }
+    
+    s = "00:04:4B:66:9F:3A";
+    if ( s != myAddr ) {
+	destinations.push_back(s);
+    }
 
-	deque<string> deviceBtAddrQ;
-	deviceBtAddrQ.push_back("00:04:4B:65:C3:75");
-	deviceBtAddrQ.push_back("00:04:4B:66:9F:3A");
-	//deviceBtAddrQ.push_back("00:00:00:00:00:00");
+    //Create the receive threads first
+    for (unsigned int i = 0; i < destinations.size(); i++) {
+	threads.push_back(std::thread(runBluetoothReceive) );
+    }
 
-	// check your own address
-	// do corresponding init work
-	Bluetooth bluetooth(deviceBtAddrQ);
-	bluetooth.setMyAddress(myAddr);
+    //We only need one send thread as we are broadcasting to all other robots
+    threads.push_back(std::thread(runBluetoothSend));
+    
+    std::unique_lock<std::mutex> lck(mtx);
+    std::string temp;
 
-	cout << "My address index is " << bluetooth.getMyAddress() << endl;
-
-	deque<string> send_msgs;
-	deque<string> received_msgs;
-	deque<string> bufferQ;
-
-	vector<thread> sendThreads, receiveThreads;
-	// ISSUE: i++
-	for (int i = deviceBtAddrQ.size() - 1, j = 1; i >= 0 ; i--) {
-		if (i != bluetooth.getMyAddress()) {
-			sendThreads.push_back(thread(runBluetoothSend, ref(send_msgs), deviceBtAddrQ[i],
-				ref(bluetooth), j));
-			receiveThreads.push_back(thread(runBluetoothReceive, ref(received_msgs),
-				ref(bluetooth), j++));
-		}
-	}
-
-	unique_lock<mutex> lck(mtx);
-	string temp;
-	while (true) {
-		lck.unlock();
-		cout << "> Say something <1 digit index num><msg>: ";
-		cin >> temp;
-		// ISSUE: better to cast?
-		int index = temp[0] - '0';
-		// ISSUE: Remove the first character
-		cout << "sending \"" << temp << "\" to #" << index << endl;
-
-		printf("Main: wake up sending thread\n");
-
-		lck.lock();
-		send_msgs.push_back(temp);
-		// wake up sending thread
-		bt_send.notify_one();
-		bt_send2.notify_one();
-		cout << "waked up!!" << endl;
-		temp.clear();
-		//main_cv.wait(lck);
-	}
-
-
-	for (int i = 0; (unsigned)i < sendThreads.size(); i++) {
-		sendThreads[i].join();
-	}
-	for (int i = 0; (unsigned)i < receiveThreads.size(); i++) {
-		receiveThreads[i].join();
-	}
-
-	return 0;
-}
-
-void runBluetoothSend(deque<string>& msgs, string dest,
-	Bluetooth& bluetooth, int threadNum)
-{
-	condition_variable &bt_sendRef = threadNum == 1 ? bt_send : bt_send2;
-	unique_lock<mutex> lck(mtx);
+    //This is just for testing purposes.
+    //TODO: Need a message processing thread
+    while (true) {
 	lck.unlock();
-	cout << "Thread: send begin" << endl;
+	std::cout << "> Say something (one word): ";
+	std::cin >> temp;
+	lck.lock();
+	send_msgs.push_back(temp);
+	
+	// wake up sending thread
+	bt_send.notify_one();
+    }
 
-	int index = bluetooth.find(dest);
-	if (index > bluetooth.getMyAddress()) {
-		// connect to lower index
-		bt_sendRef.wait(lck);
-	}
-	cout << "connect to " << dest << "....\nSuccessful? ";
-	if (!bluetooth.connect(index, threadNum)) {
-		cout << "false" << endl;
-		return;
-	}
-	cout << "true" << endl;
-
-
-	string msg;
-	msg.push_back('0' + bluetooth.getMyAddress());
-	if (bluetooth.send(index, msg) <= 0) {
-		// notify the device
-		cerr << "Failed: unable to send confirmation msg: \"" << msg << "\""
-			<< endl;
-	}
-
-	while (true) {
-		//printf("Thread: sending\n");
-		lck.lock();
-		int size = msgs.size();
-		printf("msgQ size = %d\n", size);
-		for (int i = 0; i < size; i++) {
-			int destIndex = -1;
-			try {
-				// get the first letter of the msg, which is the destnation index
-				destIndex = msgs.at(0).at(0) - '0';
-			}
-			catch (out_of_range ex) {
-				cerr << "Error: \"" << msgs[0] << "\" has invalid size = " << msgs[0].size() << endl;
-				break;
-			}
-
-			if (bluetooth.getCurrTheradNum(destIndex) == threadNum) {
-				// should send in this thread
-				if (bluetooth.send(index, msgs[0]) <= 0) {
-					cerr << "Failed: unable to send \"" << msgs[i] << "\""
-						<< endl;
-
-					// break the sending loop
-					break;
-				}
-
-				cout << "Msg sent: \"" << msgs[0] << "\"" << endl;
-			}
-			else {
-				msgs.push_back(msgs[0]);
-			}
-			msgs.pop_front();
-		}
-
-		/*if (msgs.empty()) {
-			cout << "Thread bt_send" << threadNum << ": acquire sending lock" << endl;
-			bt_sendRef.wait(lck);
-		}*/
-		cout << "Thread bt_send" << threadNum << ": acquire sending lock" << endl;
-		bt_sendRef.wait(lck);
-		lck.unlock();
-	}
+    for ( unsigned int i=0; i<threads.size(); i++ ) {
+	threads[i].join();
+    }
+    
+    return 0;
 }
 
-void runBluetoothReceive(deque<string>& msgs, Bluetooth& bluetooth,
-	int threadNum)
-{
-	unique_lock<mutex> lck(mtx);
+int getConnection(const char* dest) {    
+    sockaddr_rc addr = { 0 };
+    int s, status;
+
+    std::cout<<"Connecting to "<<dest<<std::endl;
+    
+    // allocate a socket
+    s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+
+    // set the connection parameters (who to connect to)
+    addr.rc_family = AF_BLUETOOTH;
+    addr.rc_channel = (uint8_t) 1;
+    str2ba( dest, &addr.rc_bdaddr );
+
+    // connect to server
+    status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
+    if ( status != 0 ) {
+	std::cout<<"Unable to connect to "<<dest<<std::endl;
+	return status;
+    }
+
+    return s;
+}
+
+void runBluetoothSend() {
+    //Get a connection for each of the destinations
+    int status;
+    std::vector<int> connections;
+
+    //We need to pause the send thread to give the receive threads
+    //on the other Jetsons time to get to the 'accept'
+    //In addition, we found that we couldn't have two "simultaneous"
+    //send connection requests at the same time. Through experimenting
+    //there seems to need to be at least 4 seconds between send
+    //connection requests
+    int value = 5000000 + 4000000 * (rand()%10);
+    std::cout<<"random sleep value is "<<value<<std::endl;
+    usleep(value);
+    
+    for ( unsigned int i=0; i<destinations.size(); i++ ) {
+	std::string dest = destinations[i];
+	int socket = getConnection( dest.c_str() );
+	if ( socket != -1 ) {
+	    //Connection was successful
+	    connections.push_back( socket );
+	} else {
+	    std::cout<<"Connection was not successful"<<std::endl;
+	}
+    }
+
+    //Begin the send message loop
+    std::unique_lock<std::mutex> lck(mtx);
+    lck.unlock();
+ 
+    while (true) {
+	lck.lock();
+	unsigned int size = send_msgs.size();
+	for (unsigned int i = 0; i < size; i++) {
+	    //Broadcast each message to the other robots
+	    for ( unsigned int j=0; j<connections.size(); j++ ) {
+		status = write( connections[j], (char *)send_msgs[i].c_str(), send_msgs[i].size() );
+		if ( status == 0 ) {
+		    std::cout<<"Unable to send message to destination "<<destinations[j]<<std::endl;
+		}
+	    }
+	}
+
+	//No more messages to send
+	send_msgs.clear();
+	bt_send.wait(lck);
 	lck.unlock();
-	cout << "pack_number" << threadNum << ": receive begin" << endl;
-
-	if (bluetooth.initListener(threadNum) == false) {
-		cerr << "invalid threadNum = " << threadNum << endl;
-		return;
-	}
-
-	char buffer[1024];
-	memset(buffer, '\0', 1024);
-
-	string msg;
-	while (bluetooth.listen(threadNum, msg, buffer) <= 0) {
-		printf("Failed: unable to receive data\n");
-	}
-
-	int confirmedDeviceIndex = -1;
-	try {
-		confirmedDeviceIndex = stoi(msg);
-	}
-	catch (invalid_argument ex) {
-		cerr << "confirmation message \"" << msg << "\" is invalid" << endl;
-		return;
-	}
-	catch (out_of_range ex) {
-		cerr << "confirmation message too large" << endl;
-		return;
-	}
-
-	if (confirmedDeviceIndex < 0 || confirmedDeviceIndex >= 3)
-		return;
-	condition_variable *bt_sendRef = NULL;
-	confirmedDeviceIndex = bluetooth.getMyAddress() - confirmedDeviceIndex;
-	if (confirmedDeviceIndex > 0) {
-		// myAddr = 3, index == 1: myAddr - index = 3 - 1 = 2, 2 % 2 = 0 -> bt_send
-		// myAddr = 3, index == 2: myAddr - index = 3 - 2 = 1, 1 % 2 = 1 -> bt_send2
-		// myAddr = 2, index == 1: myAddr - index = 2 - 1 = 1, 1 % 2 = 1 -> bt_send2
-		if (confirmedDeviceIndex % 2 == 1) {
-			bt_sendRef = &bt_send2;
-		}
-		else {
-			bt_sendRef = &bt_send;
-		}
-	}
-	if (bt_sendRef != NULL) {
-		bt_sendRef->notify_one();
-	}
-
-	msg.clear();
-	while (true) {
-		printf("Thread: listening: \n");
-		if (bluetooth.listen(threadNum, msg, buffer) <= 0) {
-			printf("Failed: unable to receive data\n");
-		}
-		else {
-			cout << buffer << endl;
-			lck.lock();
-			msgs.push_back(msg);
-			lck.unlock();
-		}
-		// clear the string
-		msg.clear();
-	}
+    }
 }
 
+void runBluetoothReceive() {
+    //Wait for a connection request
+    const int BUF_SIZE = 1024;
+    int error;
+    int sock;
+    char buf[BUF_SIZE] = {0};
+    sockaddr_rc local_address = {0}, remote_address = {0};
+    
+    socklen_t option = sizeof(remote_address);
+    
+    sock = socket( AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+    if ( sock == -1 ) {
+	std::cout<<"Unable to get a socket"<<std::endl;
+	return;
+    }
+    
+    local_address.rc_family = AF_BLUETOOTH;
+    local_address.rc_bdaddr = { { 0, 0, 0, 0, 0, 0 } };
+    local_address.rc_channel = (uint8_t)0;//bind to port 0 of first available bluetooth adapter
+    error = bind(sock, (struct sockaddr *)&local_address, sizeof(local_address));
+    if ( error == -1 ) {
+	std::cout<<"Error on bind"<<std::endl;
+	return;
+    }
+    
+    // put socket into listening mode
+    error = listen(sock, 1);
+    if ( error == -1 ) {
+	std::cout<<"Error on listen"<<std::endl;
+	return;
+    }
+    
+    // accept one connection
+    std::cout<<"About to call accept"<<std::endl;
+    int client = accept(sock, (struct sockaddr *)&remote_address, &option);
+    if ( client == -1 ) {
+	std::cout<<"Error on accept"<<std::endl;
+	return;
+    }
 
-void exchangeMsgs(deque<string>& destQ, deque<string>& srcQ) {
-	while (!srcQ.empty()) {
-		destQ.push_back(srcQ[0]);
-		srcQ.pop_front();
+    ba2str( &remote_address.rc_bdaddr, buf);
+    std::cout<<"Accepted connection from "<< buf << std::endl;
+    
+    std::unique_lock<std::mutex> lck(mtx);//mtx is a global lock
+    lck.unlock();
+
+    //Start our infinite message receiving loop
+    while (true) {
+	memset( buf, 0, sizeof(buf) );
+	int bytes_read = read( client, buf, BUF_SIZE );
+	std::cout<<"byte read is "<<bytes_read<<std::endl;
+	if ( bytes_read > 0 ) {
+	    //Add the message to the message queue
+	    lck.lock();
+	    received_msgs.push_back(buf);//Add the new message to the received message queue
+
+	    //TODO:Will eventually need a message processor thread that waits on the
+	    //bt_receive
+	    std::cout<<"message is:"<<buf<<std::endl;//Just for testing purposes
+	    
+	    bt_receive.notify_one();//Wake up the message handler that we have received a message
+	    lck.unlock();
 	}
-
+    }
 }
